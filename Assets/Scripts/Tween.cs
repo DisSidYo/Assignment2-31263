47fbcfddc5781d19235aac6fc52b99f202289d32
Siddharth Wani <sidwani04@gmail.com>
2025-09-22 04:03:47 +1000

    Done with almost all movement I reckon

---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween
+{
+    // Start is called before the first frame update
+    public Transform Target { get; private set; }
+
+    public Vector3 StartPos { get; private set; }
+    public Vector3 EndPos { get; private set; }
+
+    public float Duration { get; private set; }
+
+    public float StartTime { get; private set; }
+    
+    public Tween(Transform target, Vector3 startPos, Vector3 endPos, float startTime, float duration)
+    {
+        Target = target;
+        StartPos = startPos;
+        EndPos = endPos;
+        StartTime = startTime;
+        Duration = duration;
+        
+    }
+
+
+    
+    
+    
+
+}
